6c4574bef4535e37f763bc40d37bb1f1f266c25d
bobdi <prevostdjonko@gmail.com>
2019-11-09 19:58:03 -0500

    lab 8 server

---
+++ common/config-server-lab8/src/main/java/microservice/springcloug/configserver/Lab8ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package microservice.springcloug.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab8ConfigServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Lab8ConfigServerApplication.class, args);
+    }
+
+
+}
+
---
+++ common/config-server-lab8/src/test/java/microservice/springcloug/lab8configserver/Lab8ConfigServerApplicationTests.java
@@ -0,0 +1,19 @@
+package microservice.springcloug.lab8configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab8ConfigServerApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
+
