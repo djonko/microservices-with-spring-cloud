6f9ea8fa95b80ba28b1089f7b582b66d098c41a5
olarila-l635 <prevostdjonko@yahoo.fr>
2019-12-07 18:33:00 -0500

    lab 9: part 3

--- lab-9-gateway/src/main/java/microservice/springcloug/lab9gateway/Lab9GatewayApplication.java
+++ lab-9-gateway/src/main/java/microservice/springcloug/lab9gateway/Lab9GatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -13,5 +15,9 @@ public class Lab9GatewayApplication {
         SpringApplication.run(Lab9GatewayApplication.class, args);
     }
 
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter(){
+        return  new ShallowEtagHeaderFilter();
+    }
 }
 
